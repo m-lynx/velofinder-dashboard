fdb3ba8adbbe825918710ee3f6397d0dae51a55c
Maciej Mazurek <mazurek.lynx@gmail.com>
2026-09-14 18:05:22 +0200

    use prod instance for auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://dev.velofinder.pl/api',
-  siteUrl: 'https://dev.velofinder.pl',
+  apiUrl: 'https://velofinder.pl/api',
+  siteUrl: 'https://velofinder.pl',
 };
