a1267b7a5696bd5e7d5a500792ac23f5961b33e9
Maciej Mazurek <mazurek.lynx@gmail.com>
2026-09-14 14:31:55 +0200

    use dev instance for auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://velofinder.pl/api',
-  siteUrl: 'https://velofinder.pl',
+  apiUrl: 'https://dev.velofinder.pl/api',
+  siteUrl: 'https://dev.velofinder.pl',
 };
